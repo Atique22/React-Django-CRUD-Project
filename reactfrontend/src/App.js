59b5d0ef8433df0731cb717eb4d81b8cff470fce
Atique Ahmad <bsce19008@itu.edu.pk>
2023-03-28 01:31:08 +0500

    Revert "Revert "crud with video frame sets""
    This reverts commit 9f4296e5a0bd537813d94bf775514456a674a59f.

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -2,11 +2,29 @@ import "./App.css";
 import GetData from "./components/GetData";
 import VideoCapture from "./components/VideoCaptureFrame";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <GetData />
-      <VideoCapture />
+      <Router>
+        <Link to="/">
+          <Button variant="success" className=" m-2">
+            CRUD-with User Data
+          </Button>
+        </Link>
+        <Link to="/video">
+          <Button variant="success" className=" m-2">
+            CRUD-with Video Frames
+          </Button>
+        </Link>
+
+        <Routes>
+          <Route exact path="/" element={<GetData />} />
+          <Route exact path="/video" element={<VideoCapture />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
